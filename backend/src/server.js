260cdc0d3c3809ca48e31cdc04980554613cb35c
Trong Quy <trongquy1628@gmail.com>
2025-11-07 22:20:17 +0700

    completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import tasksRoutes from './routes/tasksRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+))
 
 app.use('/api/tasks', tasksRoutes);
 
